bc9d1cf02ac0c5f93412b0356ed368825e0cfa8d
delinquentxx <cmposadas@up.edu.ph>
2021-06-24 00:12:28 +0800

    second commit

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ def home():
 def forum():
     return render_template('forum.html')
 
+@app.route('/register')
+def register():
+    return render_template('reg-page1.html')
+
+@app.route('/register=page2')
+def register2():
+    return render_template('reg-page2.html')
 
 if __name__ == '__main__' :
     app.run(debug=True)
